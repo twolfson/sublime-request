9e1a2e8335e31d1013d56de31602553dadbaf03c
Todd Wolfson <todd@twolfson.com>
2013-06-21 02:28:44 -0700

    Added `save_to_clipboard` support

--- request.py
+++ request.py
@@ -1,4 +1,5 @@
 # Load in dependencies
+import sublime
 import sublime_plugin
 import urllib2
 
@@ -6,6 +7,12 @@ import urllib2
 # Setup request command for use
 class RequestCommand(sublime_plugin.WindowCommand):
     def run(self, open_args=[], open_kwargs={},
-            read_args=[], read_kwargs={}, **kwargs):
+            read_args=[], read_kwargs={}, save_to_clipboard=False,
+            *args, **kwargs):
+        # Make the request as requested
         req = urllib2.urlopen(*open_args, **open_kwargs)
-        return req.read(*read_args, **read_kwargs)
+        result = req.read(*read_args, **read_kwargs)
+
+        # If we should save result to clipboard, save it
+        if save_to_clipboard:
+            sublime.set_clipboard(result)
