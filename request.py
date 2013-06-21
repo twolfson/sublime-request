c55bc81653c7f28cb921941301f701926dc256fe
Todd Wolfson <todd@twolfson.com>
2013-06-21 02:22:42 -0700

    Backed off of `with` from weird terminal denial
    >>> window.run_command('request', {"open_args": ["http://localhost:3000/"]})
    ...
    AttributeError: addinfourl instance has no attribute '__exit__'

--- request.py
+++ request.py
@@ -5,6 +5,7 @@ import urllib2
 
 # Setup request command for use
 class RequestCommand(sublime_plugin.WindowCommand):
-    def run(self, open_args=[], open_kwargs={}, read_args=[], read_kwargs={}, **kwargs):
-        with urllib2.urlopen(*open_args, **open_kwargs) as req:
-            req.read(*read_args, **read_kwargs)
+    def run(self, open_args=[], open_kwargs={},
+            read_args=[], read_kwargs={}, **kwargs):
+        req = urllib2.urlopen(*open_args, **open_kwargs)
+        return req.read(*read_args, **read_kwargs)
