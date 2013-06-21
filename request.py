eb8982be3dc9da72234978748fa9bc1014b41129
Todd Wolfson <todd@twolfson.com>
2013-06-21 02:18:13 -0700

    Plugin working successfully

--- request.py
+++ request.py
@@ -1,2 +1,10 @@
+# Load in dependencies
 import sublime
 import sublime_plugin
+import urllib2
+
+
+class RequestCommand(sublime_plugin.WindowCommand):
+    def run(self, open_args=[], open_kwargs={}, read_args=[], read_kwargs={}, **kwargs):
+        req = urllib2.urlopen(*open_args, **open_kwargs)
+        req.read(*read_args, **read_kwargs)
