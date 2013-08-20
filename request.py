5556f200937a431ba9c6ddd758e9ca37e3c3e6c1
Todd Wolfson <todd@twolfson.com>
2013-08-20 01:44:44 -0700

    Piece-wise imports for Sublime Text 3 support

--- request.py
+++ request.py
@@ -1,7 +1,13 @@
-# Load in dependencies
+# Load in core dependencies
 import sublime
 import sublime_plugin
-import urllib2
+
+# Attempt to load urllib.request/error and fallback to urllib2 (Python 2/3 compat)
+try:
+    from urllib.request import urlopen
+    from urllib.error import URLError
+except ImportError:
+    from urllib2 import urlopen, URLError
 
 
 # Setup request command for use
@@ -15,11 +21,11 @@ class RequestCommand(sublime_plugin.WindowCommand):
         # Attempt to open the url
         try:
             # Make our open request
-            req = urllib2.urlopen(*open_args, **open_kwargs)
+            req = urlopen(*open_args, **open_kwargs)
         except TypeError as err:
             # If the arguments are malformed, display the error
             return sublime.status_message(str(err))
-        except urllib2.URLError as err:
+        except URLError as err:
             # Otherwise, if there was a connection error, let it be known
             return sublime.status_message('Error connecting to "%s"' % url)
 
@@ -32,5 +38,6 @@ class RequestCommand(sublime_plugin.WindowCommand):
 
         # If we should save result to clipboard, save it
         if save_to_clipboard:
-            sublime.set_clipboard(result)
+            # DEV: For Sublime Text 3 support, we must coerce result from bytes into a string
+            sublime.set_clipboard(str(result))
             sublime.status_message('Saved result from "%s" to clipboard' % url)
